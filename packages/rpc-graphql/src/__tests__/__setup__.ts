8a1e70c4bb3d91bdbb829f5bcdd3e9458c8dfa18
Naasir Jusab <nasjuice7@gmail.com>
2024-05-06 20:22:19 -0400

    Revert "refactor(experimental): graphql: token-2022 extensions: ConfidentialTransfer"

--- packages/rpc-graphql/src/__tests__/__setup__.ts
+++ packages/rpc-graphql/src/__tests__/__setup__.ts
@@ -2304,6 +2304,7 @@ export const mockTransactionToken2022AllExtensions = {
                     programId: 'TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb',
                     stackHeight: null,
                 },
+
                 {
                     parsed: {
                         info: {
@@ -2341,44 +2342,6 @@ export const mockTransactionToken2022AllExtensions = {
                             ],
                         },
                         type: 'withdrawConfidentialTransfer',
-                    },
-                    program: 'spl-token',
-                    programId: 'TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb',
-                    stackHeight: null,
-                },
-                {
-                    parsed: {
-                        info: {
-                            source: '2Pwe6Yahh5cbzvCwRMtTYFeboSwYiWeHhYJzZZBsU6eB',
-                            destination: '2Pwe6Yahh5cbzvCwRMtTYFeboSwYiWeHhYJzZZBsU6eB',
-                            mint: 'FsHcsGiY43QmZc6yTgwYC1DA5U3ZgycXxn3bd2oBjrEZ',
-                            instructionsSysvar: 'SysvarRent111111111111111111111111111111111',
-                            newSourceDecryptableAvailableBalance: '57fZKMs9YqFhu6fENwKTe7mGZh+wJbIvYkQ3LI/FDnOdc2w7',
-                            owner: 'FsHcsGiY43QmZc6yTgwYC1DA5U3ZgycXxn3bd2oBjrEZ',
-                            proofInstructionOffset: 1,
-                        },
-                        type: 'confidentialTransfer',
-                    },
-                    program: 'spl-token',
-                    programId: 'TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb',
-                    stackHeight: null,
-                },
-                {
-                    parsed: {
-                        info: {
-                            source: '2Pwe6Yahh5cbzvCwRMtTYFeboSwYiWeHhYJzZZBsU6eB',
-                            destination: '2Pwe6Yahh5cbzvCwRMtTYFeboSwYiWeHhYJzZZBsU6eB',
-                            mint: 'FsHcsGiY43QmZc6yTgwYC1DA5U3ZgycXxn3bd2oBjrEZ',
-                            instructionsSysvar: 'SysvarRent111111111111111111111111111111111',
-                            newSourceDecryptableAvailableBalance: '57fZKMs9YqFhu6fENwKTe7mGZh+wJbIvYkQ3LI/FDnOdc2w7',
-                            proofInstructionOffset: 1,
-                            multisigOwner: 'FsHcsGiY43QmZc6yTgwYC1DA5U3ZgycXxn3bd2oBjrEZ',
-                            signers: [
-                                '2Pwe6Yahh5cbzvCwRMtTYFeboSwYiWeHhYJzZZBsU6eB',
-                                '2Pwe6Yahh5cbzvCwRMtTYFeboSwYiWeHhYJzZZBsU6eB',
-                            ],
-                        },
-                        type: 'confidentialTransfer',
                     },
                     program: 'spl-token',
                     programId: 'TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb',
